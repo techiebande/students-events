558bac2d6eb3d75afcd41ded6c53a4391805c663
techieBande <obande5826@gmail.com>
2024-03-15 18:50:55 +0100

    add action buttons screen 11 mobile view

--- app/challenge/page.tsx
+++ app/challenge/page.tsx
@@ -7,11 +7,24 @@ import { cn, filmoType } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
 
+const Actions = () => {
+  return (
+    <div className="flex items-center gap-6">
+      <Button className="py-3 px-6 flex items-center justify-center border rounded-[6px] border-gray-300 hover:shadow-stats bg-white hover:bg-white text-gray-600 poppins-semibold text-sm leading-5">
+        Back
+      </Button>
+      <Button className="items-center justify-center w-[216px] bg-primary600 hover:bg-primary700 poppins-semibold text-sm leading-5 text-white">
+        Next
+      </Button>
+    </div>
+  );
+};
+
 const ChallengePage = () => {
   return (
     <div>
       <ChallengeHeader title="Introduction to strategy consulting " />
-      <div className="mt-5 px-2 md:px-[60px] flex gap-[30px]">
+      <div className="mt-5 px-2 md:px-[60px] flex gap-[30px] pb-6">
         <div className="w-full">
           <Paper className="">
             <p className="text-gray-800 text-sm poppins-medium leading-5">
@@ -37,6 +50,9 @@ const ChallengePage = () => {
               />
             </div>
           </Paper>
+          <div className="mt-6 lg:hidden">
+            <Actions />
+          </div>
         </div>
         <div className="hidden lg:flex w-[324px] h-full flex-col ">
           <Paper className="w-full flex items-center justify-center">
@@ -45,13 +61,8 @@ const ChallengePage = () => {
           <Paper className="mt-6 pl-6">
             <ChallengeStepsList />
           </Paper>
-          <div className="flex items-center gap-6 mt-[56px]">
-            <Button className="py-3 px-6 flex items-center justify-center border rounded-[6px] border-gray-300 hover:shadow-stats bg-white hover:bg-white text-gray-600 poppins-semibold text-sm leading-5">
-              Back
-            </Button>
-            <Button className="items-center justify-center w-[216px] bg-primary600 hover:bg-primary700 poppins-semibold text-sm leading-5 text-white">
-              Next
-            </Button>
+          <div className="mt-[53px]">
+            <Actions />
           </div>
         </div>
       </div>
